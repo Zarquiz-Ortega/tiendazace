85dcfe3101ca0319eecf7ccd66662dc2f021602d
Zarquiz-Ortega <83607679+Zarquiz-Ortega@users.noreply.github.com>
2024-03-13 13:51:32 -0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,25 +2,25 @@
 
 //configuraciones del sistema
 define("SITE_URL","http://localhost/tiendazace/tienda_zace_cliente/");
-define("KEY_TOKEN","APR.wqc-354*");
+define("KEY_TOKEN","");
 define("MONEDA","$");
 
 
 //Configuraciones Paypal
-define("CLIENT_ID","AWMtdFgQTcYR_82X9JlX6SpJxNqpKWoncN7XxxGB2Q8BIvqctlsZl2ro44yZu092zzZGRkJIFFe6N5sm");
+define("CLIENT_ID","");
 define("CURRENCY","MXN");
 
 //Configuraciones Mercado pago 
-define("TOKEN_MP","TEST-5518880316568114-120218-27c9e71f4ef8ed490f7c7284cc065ad5-313808390");
+define("TOKEN_MP","");
 define("PUBLIC_KEY_MP","");
 define("LOCALE_MP","es-MX");
 
 
 //correos
-define("MAIL_HOST","smtp.gmail.com");
-define("MAIL_USER","tiendazace@gmail.com");
-define("MAIL_PASS","bptfanyxtjncgsfq");
-define("MAIL_PORT","465");
+define("MAIL_HOST","");
+define("MAIL_USER","");
+define("MAIL_PASS","");
+define("MAIL_PORT","");
 
 session_start();
 
